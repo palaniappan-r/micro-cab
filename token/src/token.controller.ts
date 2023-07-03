dc56d573173dd3ccb3d44f5ead4054cdb992a7f7
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-04 00:53:30 +0530

    feat: add token controllers

--- token/src/token.controller.ts
+++ token/src/token.controller.ts
@@ -1,7 +1,22 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
+import { TokenService } from './token.service';
 
 @Controller('token')
 export class TokenController {
-    
+    constructor(private readonly tokenService: TokenService) { }
+
+    @MessagePattern('create_token')
+    public createToken(userId : string , role : string) : string {
+        const token : string = this.tokenService.createToken(userId , role)
+        return token
+    }
+
+    @MessagePattern('decode_token')
+    public decodeToken(token : string) : any {
+        const data : any = this.tokenService.decodeToken(token)
+        return data
+    }
+
 }
