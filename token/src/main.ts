5c2143502ea9a9451f53d6d50a321381657d89fb
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-04 23:35:26 +0530

    fix: add Validation Pipe

--- token/src/main.ts
+++ token/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { TokenModule } from './token.module';
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 let PORT = 3012
 let HOST = '0.0.0.0'
@@ -10,7 +10,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: { host: HOST, port: PORT }
   });
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   app.listen();
 
   console.log(`Token Microservice on ${HOST}:${PORT}`)
