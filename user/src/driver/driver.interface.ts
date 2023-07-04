124083ac5c98b12e04cdbd833d0ae734a744e812
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-04 23:51:34 +0530

    feat: add driver interface

--- user/src/driver/driver.interface.ts
+++ user/src/driver/driver.interface.ts
@@ -1,1 +1,14 @@
-export interface Driver {}
+import { Document } from 'mongoose';
+import { GeoJSON } from 'geojson';
+
+export interface IDriver extends Document{
+    driverId: string;
+    name: string;
+    email : string;
+    password: string;
+    balance : number;
+    status : boolean;
+    driveLoc : GeoJSON;
+    comparePassword: (password: string) => Promise<boolean>; 
+    encryptPassword: (password: string) => Promise<string>;
+}
