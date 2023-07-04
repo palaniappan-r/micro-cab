83cc7e27a81bf500900f2e252f814fd5720716f1
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-04 23:35:02 +0530

    fix: update user module

--- user/src/user.module.ts
+++ user/src/user.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
@@ -7,6 +6,5 @@ import { DriverModule } from './driver/driver.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/uber-clone'), CustomerModule, DriverModule],
-  controllers: [UserController]
 })
 export class UserModule {}
