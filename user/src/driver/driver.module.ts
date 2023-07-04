b2d37a69a339fdc5aa87179be22555108d47ce57
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-04 23:56:24 +0530

    feat: add driver module

--- user/src/driver/driver.module.ts
+++ user/src/driver/driver.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Transport, ClientsModule } from '@nestjs/microservices';
+import { DriverSchema } from './driver.schema';
 import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
 
 @Module({
-  controllers: [DriverController],
-  providers: [DriverService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Driver', schema: DriverSchema}]),
+    ClientsModule.register([
+      { name: 'TOKEN_SERVICE', transport: Transport.TCP, options: { host: '0.0.0.0', port: 3012 } }
+    ]),
+    DriverModule],
+  providers: [DriverService],
+  controllers: [DriverController]
 })
 export class DriverModule {}
