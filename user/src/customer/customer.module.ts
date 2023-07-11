afbe4478e0745289fc685efcc5982a6e0a7c7866
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-12 02:20:05 +0530

    feat: use env vars in user microservice

--- user/src/customer/customer.module.ts
+++ user/src/customer/customer.module.ts
@@ -6,11 +6,15 @@ import { CustomerSchema } from './customer.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Transport, ClientsModule } from '@nestjs/microservices';
 
+import * as dotenv from 'dotenv';
+
+const tokenServicePort = parseInt(process.env.TOKEN_SERVICE_PORT) || 3012;
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema}]),
     ClientsModule.register([
-      { name: 'TOKEN_SERVICE', transport: Transport.TCP, options: { host: '0.0.0.0', port: 3012 } }
+      { name: 'TOKEN_SERVICE', transport: Transport.TCP, options: { host: '0.0.0.0', port: tokenServicePort } }
     ]),
     CustomerModule],
   providers: [CustomerService],
--- user/src/driver/driver.module.ts
+++ user/src/driver/driver.module.ts
@@ -5,11 +5,15 @@ import { DriverSchema } from './driver.schema';
 import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
 
+import * as dotenv from 'dotenv';
+
+const tokenServicePort = parseInt(process.env.TOKEN_SERVICE_PORT) || 3012;
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Driver', schema: DriverSchema}]),
     ClientsModule.register([
-      { name: 'TOKEN_SERVICE', transport: Transport.TCP, options: { host: '0.0.0.0', port: 3012 } }
+      { name: 'TOKEN_SERVICE', transport: Transport.TCP, options: { host: '0.0.0.0', port: tokenServicePort } }
     ]),
     DriverModule],
   providers: [DriverService],
