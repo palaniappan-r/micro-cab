964a74750ce5dcf5cd9a3d1de9154740964fb6e5
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-12 01:57:45 +0530

    feat: update trip service

---
+++ trip/src/trip/trip.schema.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import { ITrip } from './trip.interface';
+
+export const TripSchema = new mongoose.Schema<ITrip>(
+  {
+    tripId:{
+        type: String
+    },
+    customer: {
+      type: Object
+    },
+    driver: {
+        type: Object
+    },
+    startPt: {
+        type: Object
+    },
+    endPt:{
+        type: Object
+    },
+    distance:{
+        type: Number
+    },
+    price:{
+        type:Number
+    },
+    createdAt:{
+        type: Date
+    },
+    updatedAt:{
+        type: Date
+    }
+  })
+
+
+
