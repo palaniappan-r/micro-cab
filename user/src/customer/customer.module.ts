7de9087b2dc82c66bd2938f759f83e59ffd0e435
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-12 01:59:01 +0530

    feat: update cust module

--- user/src/customer/customer.module.ts
+++ user/src/customer/customer.module.ts
@@ -4,6 +4,7 @@ import { CustomerController } from './customer.controller';
 import { CustomerSchema } from './customer.schema';
 
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { Transport, ClientsModule } from '@nestjs/microservices';
 
 @Module({
