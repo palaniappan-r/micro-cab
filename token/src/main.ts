8dca32e82af2b39b649109a45b597107eb822ca5
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-12 02:18:46 +0530

    feat: user env vars in token microservice

--- token/src/main.ts
+++ token/src/main.ts
@@ -3,7 +3,11 @@ import { Transport } from '@nestjs/microservices';
 import { TokenModule } from './token.module';
 import { ValidationPipe } from '@nestjs/common';
 
-let PORT = 3012
+import * as dotenv from 'dotenv';
+
+const tokenServicePort = parseInt(process.env.TOKEN_SERVICE_PORT) || 3012;
+
+let PORT = tokenServicePort
 let HOST = '0.0.0.0'
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(TokenModule, {
