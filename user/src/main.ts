b6a1440b35fd54b0a797a75451ce4e0ba28cc9b0
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-12 01:58:16 +0530

    feat: update trip microservice

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { UserModule } from './user.module';
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 let PORT = 3011
 let HOST = '0.0.0.0'
@@ -10,7 +10,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: { host: HOST, port: PORT }
   });
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   app.listen();
 
   console.log(`User Microservice on ${HOST}:${PORT}`)
