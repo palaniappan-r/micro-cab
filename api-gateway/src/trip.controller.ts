90e5f4f960c115f0b4d90fdcba77cc43190afcd9
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-18 22:46:15 +0530

    feat : add trip endpts

--- api-gateway/src/trip.controller.ts
+++ api-gateway/src/trip.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get , Inject , Post, Put ,Res , Body , HttpStatus} from '@nestjs/common';
+import { Controller, Get , Inject , Post, Put ,Res , Body , HttpStatus , UseGuards, Delete} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
@@ -7,6 +7,7 @@ import { Role } from './auth/role.enum';
 import { RolesGuard } from './auth/role.guard';
 
 import { AuthGuard } from '@nestjs/passport';
+import { response } from 'express';
 
 
 @Controller('trip')
@@ -14,7 +15,8 @@ export class TripController {
   constructor(
     @Inject('TRIP_SERVICE') private readonly tripServiceClient: ClientProxy
     ) {}
-  
+ 
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
   @HasRoles(Role.CUSTOMER)
   @Post('/create')
   public async createTripCustomer(@Res() response, @Body() createTripCustomerDto) : Promise<any>{
@@ -26,6 +28,7 @@ export class TripController {
     }
   }
   
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
   @HasRoles(Role.DRIVER)
   @Get('/open')
   public async getOpenTrips(@Res() response) : Promise<any>{
@@ -38,6 +41,7 @@ export class TripController {
     }
   }
 
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
   @HasRoles(Role.CUSTOMER)
   @Put('/update')
   public async updateTripCustomer(@Res() response , @Body() updateTripCustomerDto) : Promise<any> {
@@ -48,5 +52,31 @@ export class TripController {
       data
       });
   }
+  }
+
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
+  @HasRoles(Role.DRIVER)
+  @Post('/accept')
+  public async acceptTripDriver(@Res() response , @Body() acceptTripDriverDto) : Promise<any> {
+    const data = await firstValueFrom(this.tripServiceClient.send('accept_trip_driver' , acceptTripDriverDto))
+    {
+      return response.status(HttpStatus.CREATED).json({
+        message : 'Driver assigned to trip',
+        data
+      })
+    }
+  }
+
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
+  @HasRoles(Role.DRIVER)
+  @Delete('/cancel')
+  public async cancelTripDriver(@Res() response , @Body() cancelTripDriverDto) : Promise<any> {
+    const data = await firstValueFrom(this.tripServiceClient.send('cancel_trip_driver' , cancelTripDriverDto))
+    {
+      return response.status(HttpStatus.CREATED).json({
+        message : 'Driver cancelled trip',
+        data
+      })
+    }
   }
 }
