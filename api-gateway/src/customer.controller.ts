6521882dee28aa43362ed8576298754a1ca57232
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-18 22:08:59 +0530

    feat : add role based auth for cust endpts

--- api-gateway/src/customer.controller.ts
+++ api-gateway/src/customer.controller.ts
@@ -1,42 +1,20 @@
-import { Controller, Get , Inject , Post , Put , Delete, Res , Body , HttpStatus , Param, Query} from '@nestjs/common';
+import { Controller, Get , Inject , Post , Put , Delete, Res , Body , HttpStatus , Param, Query , UseGuards} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
+import { HasRoles } from './auth/roles.decorator';
+import { Role } from './auth/role.enum';
+import { RolesGuard } from './auth/role.guard';
+
+import { AuthGuard } from '@nestjs/passport';
+
+
+@HasRoles(Role.CUSTOMER)
 @Controller('customer')
 export class CustomerController {
   constructor(
     @Inject('USER_SERVICE') private readonly userServiceClient: ClientProxy
     ) {}
-
-  @Get('/login')
-  public async test(@Res() response , @Query() params : any): Promise<any> {
-    let data = await firstValueFrom(this.userServiceClient.send('login_cust' , params.customerId))
-    {
-      return response.status(HttpStatus.FOUND).json({
-        message: 'Customer has been logged in',
-        data});
-    }
-  }
-  
-  @Get('/getAll')
-  public async getAllCustomers(@Res() response): Promise<any> {
-    let data = await firstValueFrom(this.userServiceClient.send('get_all_cust' , ""))
-    {
-      return response.status(HttpStatus.FOUND).json({
-        message: 'Customers have been found',
-        data});
-    }
-  }
-
-  @Get('/findById')
-  public async getCustomer(@Res() response , @Query() params : any): Promise<any> {
-    let data = await firstValueFrom(this.userServiceClient.send('get_cust' , params.customerId))
-    {
-      return response.status(HttpStatus.FOUND).json({
-        message: 'Customer has been found',
-        data});
-    }
-  }
 
   @Post('/create')
   public async createStudent(@Res() response, @Body() createCustomerDto) : Promise<any>{
@@ -48,7 +26,39 @@ export class CustomerController {
     }
   }
 
-  //To-Do : only update the logged in user
+  @Get('/login')
+  public async test(@Res() response , @Query() params : any): Promise<any> {
+    let data = await firstValueFrom(this.userServiceClient.send('login_cust' , params.customerId))
+    {
+      return response.status(HttpStatus.FOUND).json({
+        message: 'Customer has been logged in',
+        data});
+    }
+  }
+  
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
+  @Get('/getAll')
+  public async getAllCustomers(@Res() response): Promise<any> {
+    let data = await firstValueFrom(this.userServiceClient.send('get_all_cust' , ""))
+    {
+      return response.status(HttpStatus.FOUND).json({
+        message: 'Customers have been found',
+        data});
+    }
+  }
+
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
+  @Get('/findById')
+  public async getCustomer(@Res() response , @Query() params : any): Promise<any> {
+    let data = await firstValueFrom(this.userServiceClient.send('get_cust' , params.customerId))
+    {
+      return response.status(HttpStatus.FOUND).json({
+        message: 'Customer has been found',
+        data});
+    }
+  }
+
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
   @Put('/update')
   public async updateCustomerById(@Res() response, @Body() updateCustomerDto , @Query() params : any) : Promise<any>{
     //For now, the customerId is passed as a query param, later, it'll be taken from the auth cookie
@@ -62,6 +72,7 @@ export class CustomerController {
   }
 
   //To-Do : Chk if user is logged in first
+  @UseGuards(AuthGuard('jwt') , RolesGuard)
   @Delete('/delete')
   public async deleteCustomerBuId(@Res() response, @Query() params : any) : Promise<any>{
     let data = await firstValueFrom(this.userServiceClient.send('delete_cust' , params.customerId))
