546b24e2878267e364dc83a1a58e998d80798250
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-19 02:39:04 +0530

    feat : get customer and driverId from the token

--- api-gateway/src/trip.controller.ts
+++ api-gateway/src/trip.controller.ts
@@ -5,21 +5,24 @@ import { firstValueFrom } from 'rxjs';
 import { HasRoles } from './auth/roles.decorator';
 import { Role } from './auth/role.enum';
 import { RolesGuard } from './auth/role.guard';
-
+import { UserService } from './auth/user.service';
 import { AuthGuard } from '@nestjs/passport';
-import { response } from 'express';
 
 
 @Controller('trip')
 export class TripController {
   constructor(
-    @Inject('TRIP_SERVICE') private readonly tripServiceClient: ClientProxy
+    @Inject('TRIP_SERVICE') private readonly tripServiceClient: ClientProxy,
+    private userService : UserService
     ) {}
  
   @UseGuards(AuthGuard('jwt') , RolesGuard)
   @HasRoles(Role.CUSTOMER)
   @Post('/create')
   public async createTripCustomer(@Res() response, @Body() createTripCustomerDto) : Promise<any>{
+    const user = this.userService.getUser()
+    createTripCustomerDto.customerId = user.userId
+
     const data = await firstValueFrom(this.tripServiceClient.send('create_trip_cust' , createTripCustomerDto))
     {
       return response.status(HttpStatus.CREATED).json({
@@ -45,6 +48,9 @@ export class TripController {
   @HasRoles(Role.CUSTOMER)
   @Put('/update')
   public async updateTripCustomer(@Res() response , @Body() updateTripCustomerDto) : Promise<any> {
+    const user = this.userService.getUser()
+    updateTripCustomerDto.customerId = user.userId
+
     const data = await firstValueFrom(this.tripServiceClient.send('update_trip_cust' , updateTripCustomerDto))
   {
     return response.status(HttpStatus.CREATED).json({
@@ -58,6 +64,9 @@ export class TripController {
   @HasRoles(Role.DRIVER)
   @Post('/accept')
   public async acceptTripDriver(@Res() response , @Body() acceptTripDriverDto) : Promise<any> {
+    const user = this.userService.getUser()
+    acceptTripDriverDto.driverId = user.userId
+
     const data = await firstValueFrom(this.tripServiceClient.send('accept_trip_driver' , acceptTripDriverDto))
     {
       return response.status(HttpStatus.CREATED).json({
@@ -71,6 +80,9 @@ export class TripController {
   @HasRoles(Role.DRIVER)
   @Delete('/end')
   public async endTripDriver(@Res() response , @Body() endTripDriverDto) : Promise<any> {
+    const user = this.userService.getUser()
+    endTripDriverDto.driverId = user.userId
+
     const data = await firstValueFrom(this.tripServiceClient.send('end_trip_driver' , endTripDriverDto))
     {
       return response.status(HttpStatus.CREATED).json({
@@ -84,6 +96,9 @@ export class TripController {
   @HasRoles(Role.DRIVER)
   @Delete('/cancel')
   public async cancelTripDriver(@Res() response , @Body() cancelTripDriverDto) : Promise<any> {
+    const user = this.userService.getUser()
+    cancelTripDriverDto.driverId = user.userId
+
     const data = await firstValueFrom(this.tripServiceClient.send('cancel_trip_driver' , cancelTripDriverDto))
     {
       return response.status(HttpStatus.CREATED).json({
