3fbca976a8802b816019818798c34a6ac624ff52
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-19 02:39:19 +0530

    chore : clean up console.log

--- api-gateway/src/auth/role.guard.ts
+++ api-gateway/src/auth/role.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from './role.enum';
+import { UserService } from './user.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector , private userService: UserService) {}
 
   canActivate(context: ExecutionContext): boolean {
-    console.log("hello")
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
       context.getHandler(),
       context.getClass(),
@@ -16,7 +16,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    console.log(user)
+    this.userService.setUser(user)
     return requiredRoles.some((role) => user?.roles?.includes(role));
   }
 }
