83ec777b044c51fc7cc652726692cd7c560bc87e
palaniappan-r <palaniappan.r.mail@gmail.com>
2023-07-18 22:09:26 +0530

    feat : add role based auth for trip endpts

--- api-gateway/src/trip.controller.ts
+++ api-gateway/src/trip.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get , Inject , Post, Put ,Res , Body , HttpStatus} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { response } from 'express';
 import { firstValueFrom } from 'rxjs';
+
+import { HasRoles } from './auth/roles.decorator';
+import { Role } from './auth/role.enum';
+import { RolesGuard } from './auth/role.guard';
+
+import { AuthGuard } from '@nestjs/passport';
+
 
 @Controller('trip')
 export class TripController {
   constructor(
     @Inject('TRIP_SERVICE') private readonly tripServiceClient: ClientProxy
     ) {}
-
+  
+  @HasRoles(Role.CUSTOMER)
   @Post('/create')
   public async createTripCustomer(@Res() response, @Body() createTripCustomerDto) : Promise<any>{
     const data = await firstValueFrom(this.tripServiceClient.send('create_trip_cust' , createTripCustomerDto))
@@ -18,7 +25,8 @@ export class TripController {
         });
     }
   }
-
+  
+  @HasRoles(Role.DRIVER)
   @Get('/open')
   public async getOpenTrips(@Res() response) : Promise<any>{
     const data = await firstValueFrom(this.tripServiceClient.send('get_open_trips' , ""))
@@ -30,6 +38,7 @@ export class TripController {
     }
   }
 
+  @HasRoles(Role.CUSTOMER)
   @Put('/update')
   public async updateTripCustomer(@Res() response , @Body() updateTripCustomerDto) : Promise<any> {
     const data = await firstValueFrom(this.tripServiceClient.send('update_trip_cust' , updateTripCustomerDto))
